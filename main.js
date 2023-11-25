9c6dc8700c1993b6b25ba771a5b7d66845fb5f45
triiizy <151976179+triiizy@users.noreply.github.com>
2023-11-24 21:38:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const btToggleMenu = document.querySelector(".menu-hamburger")
+
+btToggleMenu.addEventListener("click", () => {
+  document.body.classList.toggle("open-nav")
+})
+
+const dialog = document.querySelector("dialog")
+const btDialog = document.querySelector(".bt-dialog")
+const btModal = document.querySelector(".bt-modal")
+
+btDialog.addEventListener('click', () => dialog.show())
+btModal.addEventListener('click', () => dialog.showModal())
+
+document.querySelectorAll("dialog button").forEach(
+  bt => bt.addEventListener("click", () => dialog.close()
+  )
+)
+
+
+
+
+
+
+
